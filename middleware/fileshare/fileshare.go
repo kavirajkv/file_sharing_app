5b10ca37c1c96fa56cfd9b2905193b1b55c43cd8
kavirajkv <kavirajk36kv@gmail.com>
2025-03-28 18:40:42 +0530

    optimzed uploadfile api using go routine

--- middleware/fileshare/fileshare.go
+++ middleware/fileshare/fileshare.go
@@ -13,15 +13,15 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 )
 
-//function to check server status
+// function to check server status
 func Status(w http.ResponseWriter, r *http.Request) {
-	msg:=Response{Message: "Server is up and running"}
+	msg := Response{Message: "Server is up and running"}
 	json.NewEncoder(w).Encode(msg)
 }
 
-//function to upload file
+// function to upload file
 func Uploadfile(w http.ResponseWriter, r *http.Request) {
-	db:=db.ConnectDB()
+	db := db.ConnectDB()
 	defer db.Close()
 
 	file, header, err := r.FormFile("file")
@@ -32,33 +32,68 @@ func Uploadfile(w http.ResponseWriter, r *http.Request) {
 	defer file.Close()
 
 	//username fetched from context send from authenticaion middleware
-	username,ok:=r.Context().Value("username").(string)
+	username, ok := r.Context().Value("username").(string)
 	if !ok {
 		http.Error(w, "Username not found in context", http.StatusInternalServerError)
 		return
 	}
 
+	userchan := make(chan int)
+	filechan := make(chan *manager.UploadOutput)
+	errchan := make(chan error, 2)
 
-	uploader := manager.NewUploader(S3Client)
 
-	result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
-		Bucket: aws.String(os.Getenv("AWS_S3_BUCKET")),
-		Key:    aws.String(header.Filename),
-		Body:   file,
-	})
+	go func() {
 
-	//expire after 2 days (s3 lifecycle policy created to delete files after 2 days)
-	uploadedat:=time.Now()
-	expiresat:=time.Now().Add(time.Hour * 48)
+		row := db.QueryRow(`SELECT userid FROM users WHERE username=$1`, username)
+		var userid int
+		err = row.Scan(&userid)
+		if err != nil {
+			errchan <- err
+			return
+		}
+		userchan <- userid
+	}()
 
-	row:=db.QueryRow(`SELECT userid FROM users WHERE username=$1`, username)
+	go func() {
+		uploader := manager.NewUploader(S3Client)
+
+		result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
+			Bucket: aws.String(os.Getenv("AWS_S3_BUCKET")),
+			Key:    aws.String(header.Filename),
+			Body:   file,
+		})
+		if err != nil {
+			errchan <- err
+			return
+		}
+		filechan <- result
+	}()
 	
 	var userid int
-	err = row.Scan(&userid)
+	var result *manager.UploadOutput
+
+	//Ref:- go by example (handling multiple channels)
+	for i := 0; i < 2; i++ {
+        select {
+		case user := <-userchan:
+			userid = user
+        case file := <-filechan:
+			result = file
+		case er:= <-errchan:
+			err = er
+        }
+    }
+
 	if err != nil {
-		http.Error(w, "Internal error while fetching userid", http.StatusInternalServerError)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
+	
+
+	//expire after 2 days (s3 lifecycle policy created to delete files after 2 days)
+	uploadedat := time.Now()
+	expiresat := time.Now().Add(time.Hour * 48)
 
 	//inserting file meta data into database
 	_, err = db.Exec(`INSERT INTO userfiles (filename, url, uploaded_at, expiry_at, filesize, userid) VALUES ($1,$2,$3,$4,$5,$6)`, header.Filename, result.Location, uploadedat, expiresat, header.Size, userid)
@@ -67,7 +102,7 @@ func Uploadfile(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	msg:=FileUploadResponse{Message: "File uploaded successfully", Url: result.Location, Filename: header.Filename,UploadedAt: uploadedat, Expiresat: expiresat, Size: header.Size,Userid: userid}
+	msg := FileUploadResponse{Message: "File uploaded successfully", Url: result.Location, Filename: header.Filename, UploadedAt: uploadedat, Expiresat: expiresat, Size: header.Size, Userid: userid}
 
 	json.NewEncoder(w).Encode(msg)
 }
